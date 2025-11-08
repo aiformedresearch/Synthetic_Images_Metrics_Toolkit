4ae96fa762c83709679990691039d4d14e3d062a
MatteoLai <matteo.lai3@unibo.it>
2025-11-08 17:07:02 +0100

    Updated datasets/init.py

--- sim_toolkit/datasets/init.py
+++ sim_toolkit/datasets/init.py
@@ -1,8 +1,7 @@
 # SPDX-FileCopyrightText: 2025 Matteo Lai <matteo.lai3@unibo.it>
 # SPDX-License-Identifier: NPOSL-3.0
  
-from .base import BaseDataset
-from .base3D import BidsDataset
+from .base import BaseDataset, BidsDataset
 from .nifti import NiftiDataset2D
 from .nifti import NiftiDataset3D
 from .jpeg import JPEGDataset
